d1bbabbed027473c9a399e910bf4403745b8873d
alakuntlavasu <alakuntlavasu2020@gmail.com>
2023-06-03 13:08:35 +0530

    okk

---
+++ src/Predefin_Functional_interfaces/Predicate.java
@@ -0,0 +1,12 @@
+package Predefin_Functional_interfaces;
+
+public class Predicate {
+
+}
+//1 what is predicate?
+// * The term "predicate"implies a statement that evaluates whether a value is true or false 
+//2 what is predicate interface?
+// * The predicate in java 8 is one of the predefine functional interfaces
+// * which contains the ondly single Abstract method i.e test()
+// which returen the boolen value
+// which is avilable in java.util.Function
---
+++ src/Predefin_Functional_interfaces/Predicate_Ex1.java
@@ -0,0 +1,11 @@
+package Predefin_Functional_interfaces;
+import java.util.function.Predicate;
+public class Predicate_Ex1 {
+
+	public static void main(String[] args) {
+		
+      Predicate<Integer> p=i->(i>10);
+      System.out.println(p.test(5));
+	}
+
+}
